e2885d0fcd25ae27b929a806b973df26ca5a1a29
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-12-08 23:11:15 -0300

    adicionando o tratamento das classes dos botões ao pressionar espaço ou enter

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 (() => {
 	const audioButtons = document.querySelectorAll("[data-tecla]");
 	
-	audioButtons.forEach(button => button.addEventListener("click", () => {
-		const value = button.dataset.tecla;
+	audioButtons.forEach(button => {
+		button.addEventListener("click", () => {
+			const value = button.dataset.tecla;
+			
+			const audio = document.getElementById(`som_${value}`);
+			audio.play();
+		})
 		
-		const audio = document.getElementById(`som_${value}`);
-		audio.play();
-	}));
+		button.addEventListener("keydown", (e) => {
+			const keyCode = e.keyCode
+			
+			if(keyCode === 13 || keyCode === 32) {
+				button.classList.add("ativa")
+			}
+		})
+		
+		button.addEventListener("keyup", (e) => {
+			button.classList.remove("ativa")
+		})
+	});
 })()
